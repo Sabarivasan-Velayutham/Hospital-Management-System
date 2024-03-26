2e637da0ebcbcc124c3590bce147ac9d29e68608
Sabarivasan-Velayutham <sabarivasangithub@gmail.com>
2024-03-26 17:35:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hospital Management System using MERN Stack
+
+<img src="images/1.jpg">
+
+<img src="images/2.jpg">
+
+<img src="images/3.jpg">
---
+++ frontend/src/page/Hospital1.jsx
@@ -0,0 +1,125 @@
+import human1 from "../assets/human1.jpg";
+import human4 from "../assets/human4.jpg";
+import doct1 from "../assets/doct1.jpg";
+import doct3 from "../assets/doct3.jpg";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import Navbar from "../components/Shared/Navbar";
+
+function Hospital1() {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+
+  return (
+    <div className="bg-[#FEFAE0] ">
+      <Navbar />
+      <motion.section
+        ref={ref}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: inView ? 1 : 0 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1 }}
+      >
+        <div className="h-full max-w-7xl flex flex-col m-auto justify-center items-center overflow-auto">
+          <p className="font-semibold text-3xl pt-16">Meet Our Specialists</p>
+
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-3 ">
+              <div className="w-[270px] h-[300px] border border-gray-200 rounded-lg bg-[#D4A373] shadow flex justify-center">
+                <div className="flex flex-col items-center justify-center ">
+                  <img
+                    className="w-24 h-24 mb-3 rounded-full shadow-lg"
+                    src={human1}
+                    alt=""
+                  />
+                  <h5 className="mb-1 text-xl font-medium text-black">
+                    Dr.Rahul Singh
+                  </h5>
+                  <span className="text-sm text-black">Surgeon</span>
+                  <div className="flex mt-4 md:mt-6">
+                    <a
+                      href="/appointment"
+                      className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3"
+                    >
+                      Book Appointment
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-[270px] h-[300px] bg-[#D4A373] border border-gray-200 rounded-lg shadow flex justify-center">
+                <div className="flex flex-col items-center justify-center ">
+                  <img
+                    className="w-24 h-24 mb-3 rounded-full shadow-lg"
+                    src={doct1}
+                    alt=""
+                  />
+                  <h5 className="mb-1 text-xl font-medium text-black">
+                    Dr.Ramesh Chand
+                  </h5>
+                  <span className="text-sm text-black">Surgeon</span>
+                  <div className="flex mt-4 md:mt-6">
+                    <a
+                      href="/appointment"
+                      className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3"
+                    >
+                      Book Appointment
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-[270px] h-[300px] bg-[#D4A373] border border-gray-200 rounded-lg shadow flex justify-center">
+                <div className="flex flex-col items-center justify-center ">
+                  <img
+                    className="w-24 h-24 mb-3 rounded-full shadow-lg"
+                    src={doct3}
+                    alt=""
+                  />
+                  <h5 className="mb-1 text-xl font-medium text-black">
+                    Dr.Maxwell Honda
+                  </h5>
+                  <span className="text-sm text-black">Surgeon</span>
+                  <div className="flex mt-4 md:mt-6">
+                    <a
+                      href="/appointment"
+                      className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3"
+                    >
+                      Book Appointment
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-[270px] h-[300px] bg-[#D4A373] border border-gray-200 rounded-lg shadow flex justify-center">
+                <div className="flex flex-col items-center justify-center ">
+                  <img
+                    className="w-24 h-24 mb-3 rounded-full shadow-lg"
+                    src={human4}
+                    alt=""
+                  />
+                  <h5 className="mb-1 text-xl font-medium text-black">
+                    Dr.Selena Gomez
+                  </h5>
+                  <span className="text-sm text-black">Surgeon</span>
+                  <div className="flex mt-4 md:mt-6">
+                    <a
+                      href="/appointment"
+                      className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3"
+                    >
+                      Book Appointment
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.section>
+    </div>
+  );
+}
+
+export default Hospital1;
